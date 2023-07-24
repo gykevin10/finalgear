4500b2762db7d571415f23e6dbb01965965f53f6
Kevin GY <k.gy@laposte.net>
2023-07-24 17:09:51 +0200

    eibhei

--- finalgear-front/src/pages/theory.jsx
+++ finalgear-front/src/pages/theory.jsx
@@ -7,9 +7,7 @@ const theory = () => {
       <div className="h-screen bg-cover bg-no-repeat bg-center bg-[url('/background/crisiscorereunion.png')]">
         <div className="flex justify-center p-2">
           <ImageComponent className="w-8" src="/icons/chronos.jpg" />
-          <h1 className="font-bold text-2xl underline text-center">
-            THEORIES👀
-          </h1>
+          <h1 className="font-bold text-2xl underline text-center">THEORIES</h1>
           <ImageComponent className="w-8" src="/icons/chronosl.jpeg" />
         </div>
       </div>
